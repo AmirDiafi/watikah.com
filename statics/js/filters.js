e3e3fe219b7dfe4748790f5fe2f694b6f793ba88
Amir diafi <amirdiafi2@gmail.com>
2020-05-30 12:07:03 +0100

    new modified

--- statics/js/filters.js
+++ statics/js/filters.js
@@ -25,60 +25,62 @@ $(document).ready(function() {
     })
 
     // *** Start the jquery animation effect *** //
-    $(document).ready(function() {
-        $('.status .trim i.desc').on('click', function () {
-            $(this).toggleClass('fa-caret-down fa-caret-up');
-            $(this).parent('.trim').find('p.desc').slideToggle();
-        })
-        $('.status .trim i.comment').on('click', function () {
-            $(this).toggleClass('commetnit');
-            $(this).parent('.trim').find('div.comments').slideToggle();
-        })
-
-        // *** Start Show/Hide edit post *** //
-        $('i.edit').on('click', function() {
-            $(this).parent('.status').find('ul.edit').slideToggle(500);
-        })
-        $('.container').on('click', function() {
-            $('ul.edit').fadeOut(500)
-            $('.trash-btn').fadeOut(500)
-        })
-        $('.status ul.edit, .status i.edit, .remove-cmnt-edit-icon').on('click', function(e) {
-            e.stopPropagation()
-        })
-
-        //  *** Start Search for users *** //
-        $(".search-post form input[type='search']").on('keyup', function () {
-            let user = $(this).val().toLowerCase();
-            $('.post.status').filter(function () {
-                $(this).toggle($(this).find('p.model').text().toLowerCase().search(user) >=0);
-            })
-        })
-        // *** Start show/hide remove comment button *** //
-        $('.remove-cmnt-edit-icon').on('click', function() {
-            $(this).next('.trash-btn').slideToggle()
-        })
-        //*** nitialization Nicescroll *** //
-        // $('#theusers, .popup-inner').niceScroll({
-        //     zindex:999,
-        //     cursorcolor: '#004085',
-        //     cursorborder: "none",
-        //     cursorborderradius: 0,
-        //     cursorwidth: 7,
-        // })
-
-        // *** Trim the name of file *** //
-        function TrimText(selector, maxLength) {
-            $(selector).each(function () {
-                if ($(this).text().length > maxLength) {
-                    var TrimmedText = $(this).text().slice(0, maxLength);
-                    $(this).text(TrimmedText + "...")
-                }
-            });
-        };
-    
-        TrimText(".file-content h3", 25);
-
+    $('.status .trim i.desc').on('click', function () {
+        $(this).toggleClass('fa-caret-down fa-caret-up');
+        $(this).parent('.trim').find('p.desc').slideToggle();
+    })
+    $('.status .trim i.comment').on('click', function () {
+        $(this).toggleClass('commetnit');
+        $(this).parent('.trim').find('div.comments').slideToggle();
     })
 
+    // *** Start Show/Hide edit post *** //
+    $('i.edit').on('click', function() {
+        $(this).parent('.status').find('ul.edit').slideToggle(500);
+    })
+    $('.container').on('click', function() {
+        $('ul.edit').fadeOut(500)
+        $('.trash-btn').fadeOut(500)
+    })
+    $('.status ul.edit, .status i.edit, .remove-cmnt-edit-icon').on('click', function(e) {
+        e.stopPropagation()
+    })
+
+    //  *** Start Search for users *** //
+    $(".search-post form input[type='search']").on('keyup', function () {
+        let user = $(this).val().toLowerCase();
+        $('.post.status').filter(function () {
+            $(this).toggle($(this).find('p.model').text().toLowerCase().search(user) >=0);
+        })
+    })
+    // *** Start show/hide remove comment button *** //
+    $('.remove-cmnt-edit-icon').on('click', function() {
+        $(this).next('.trash-btn').slideToggle()
+    })
+    //*** nitialization Nicescroll *** //
+    // $('#theusers, .popup-inner').niceScroll({
+    //     zindex:999,
+    //     cursorcolor: '#004085',
+    //     cursorborder: "none",
+    //     cursorborderradius: 0,
+    //     cursorwidth: 7,
+    // })
+
+    // *** Trim the name of file *** //
+    function TrimText(selector, maxLength) {
+        $(selector).each(function () {
+            if ($(this).text().length > maxLength) {
+                var TrimmedText = $(this).text().slice(0, maxLength);
+                $(this).text(TrimmedText + "...")
+            }
+        });
+    };
+
+    TrimText(".file-content h3", 25);
+
+    // *** Call the plugin *** //
+    $('.slider').slick({
+        accessisibility: true,
+        adaptiveHieght: false
+    })
 })
